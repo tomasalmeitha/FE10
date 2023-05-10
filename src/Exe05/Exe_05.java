8b41863b0504730e183bec99d72909d847b98649
Tomás <almeida.de.tomas@gmail.com>
2023-05-10 16:51:13 +0100

    Exe05 finished

--- src/Exe05/Exe_05.java
+++ src/Exe05/Exe_05.java
@@ -4,10 +4,16 @@ public class Exe_05 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Pessoa vitor = new Pessoa("Vitor Santos", "vitorsantos@gmail.com", 23, 911233344);
-        Pessoa helder = new Pessoa("Helder Pinto", "helderpinto@gmail.com", 37, 911233446);
-        Pessoa bruno = new Pessoa("Bruno Santos", "brunosantos@gmail.com", 38, 911233555);
+        Pessoa vitor = new Pessoa("Vitor Santos", 23, "vitorsantos@gmail.com", 911233344);
+        Pessoa helder = new Pessoa("Helder Pinto", 37, "helderpinto@gmail.com", 911233446);
+        Pessoa bruno = new Pessoa("Bruno Santos", 38, "brunosantos@gmail.com", 911233555);
 
-        Agenda vitorl = new Agenda(vitor);
+        Agenda agenda = new Agenda();
+
+        agenda.adicionarPessoa(vitor);
+        agenda.adicionarPessoa(helder);
+        agenda.adicionarPessoa(bruno);
+
+        agenda.listarPessoas();
     }
 }
--- src/Exe05/Pessoa.java
+++ src/Exe05/Pessoa.java
@@ -1,17 +1,55 @@
 package Exe05;
 
+import java.util.ArrayList;
 
 public class Pessoa {
+    private String nome;
+    private int idade;
+    private String email;
+    private int telemovel;
 
-    private String nome, email;
-    private int idade, telemovel;
-
-    public Pessoa(String nome, String email, int idade, int telemovel) {
-        this.nome=nome;
-        this.email=email;
-        this.idade=idade;
-        this.telemovel=telemovel;
+    public Pessoa(String nome, int idade, String email, int telemovel) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+        this.telemovel = telemovel;
     }
 
+    public String getNome() {
+        return nome;
+    }
 
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getTelemovel() {
+        return telemovel;
+    }
+}
+
+class Agenda {
+    private ArrayList<Pessoa> pessoas;
+
+    public Agenda() {
+        pessoas = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(Pessoa pessoa) {
+        pessoas.add(pessoa);
+    }
+
+    public void listarPessoas() {
+        for (Pessoa pessoa : pessoas) {
+            System.out.println("Nome: " + pessoa.getNome());
+            System.out.println("Idade: " + pessoa.getIdade());
+            System.out.println("Email: " + pessoa.getEmail());
+            System.out.println("Telefone: " + pessoa.getTelemovel());
+            System.out.println("-----------------------");
+        }
+    }
 }
