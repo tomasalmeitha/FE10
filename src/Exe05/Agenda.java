a6044858fd92d56da30ce9e9259e6b403f3e0208
Tomás <almeida.de.tomas@gmail.com>
2023-05-10 14:40:24 +0100

    minor additions to Exe05

--- src/Exe05/Agenda.java
+++ src/Exe05/Agenda.java
@@ -2,12 +2,18 @@ package Exe05;
 
 import Exe05.Pessoa;
 
+import java.util.ArrayList;
+
 public class Agenda {
 
 
-    Pessoa vitor = new Pessoa("Vitor Santos", "vitorsantos@gmail.com", 23, 911233344);
-    Pessoa helder = new Pessoa("Helder Pinto", "helderpinto@gmail.com", 37, 911233446);
-    Pessoa bruno = new Pessoa("Bruno Santos", "brunosantos@gmail.com", 38, 911233555);
+    ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
 
+    public Agenda(Pessoa x) {
+        pessoas.add(x);
+    }
 
+    public void listPessoas(){
+
+    }
 }
--- src/Exe05/Exe_05.java
+++ src/Exe05/Exe_05.java
@@ -4,7 +4,10 @@ public class Exe_05 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        Pessoa vitor = new Pessoa("Vitor Santos", "vitorsantos@gmail.com", 23, 911233344);
+        Pessoa helder = new Pessoa("Helder Pinto", "helderpinto@gmail.com", 37, 911233446);
+        Pessoa bruno = new Pessoa("Bruno Santos", "brunosantos@gmail.com", 38, 911233555);
 
-
+        Agenda vitorl = new Agenda(vitor);
     }
 }
